ab79e859227521b2a0d0c226f52f71583c831a44
Davius <dwsdavid@hotmail.com>
2024-02-12 13:35:28 +0100

    Añadida clase de entidad

---
+++ videogames/domain/VideoGames.java
@@ -0,0 +1,28 @@
+package videogames.domain;
+
+//@author DAVIUS
+
+public class VideoGames {
+    private String name;
+    
+    public VideoGames() {  
+    }
+
+    public VideoGames(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "VideoGames{" + "name=" + name + '}';
+    }
+    
+}
